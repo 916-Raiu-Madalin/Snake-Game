16c96cbc12ab0428960f3e40a6843449f7ae16b5
916-Raiu-Madalin <raiumadalin@yahoo.com>
2021-01-31 13:12:18 +0200

    partially working

--- grid/grid.py
+++ grid/grid.py
@@ -2,53 +2,136 @@ from random import randint
 
 
 class Grid:
-    def __init__(self,dimension,apples):
+    def __init__(self, dimension, apples):
         self.grid = []
-        self._dimension= dimension
+        self._dimension = dimension
         self._apples = apples
         for y in range(0, dimension):
             row = []
             for x in range(0, dimension):
                 row.append("")
             self.grid.append(row)
-        self.grid[self._dimension//2 -1][self._dimension//2 ] ='*'
-        self.grid[self._dimension//2 ][self._dimension//2 ] ='+'
-        self.grid[self._dimension//2 +1][self._dimension//2 ] ='+'
+
+        self.grid[self._dimension // 2 - 1][self._dimension // 2] = '*'
+        self.grid[self._dimension // 2][self._dimension // 2] = '+'
+        self.grid[self._dimension // 2 + 1][self._dimension // 2] = '+'
 
         x = self._apples
         while x > 0:
-            a= randint(0,self._dimension-1)
-            b= randint(0,self._dimension-1)
-            if self.grid[a][b] =="":
-                if a>0 and a <(self._dimension-1) and b>0 and b <(self._dimension-1):
-                    if self.grid[a+1][b] !="." and self.grid[a][b+1] !="." and self.grid[a-1][b] !="." and self.grid[a][b-1] !=".":
+            a = randint(0, self._dimension - 1)
+            b = randint(0, self._dimension - 1)
+            if self.grid[a][b] == "":
+                if a > 0 and a < (self._dimension - 1) and b > 0 and b < (self._dimension - 1):
+                    if self.grid[a + 1][b] != "." and self.grid[a][b + 1] != "." and self.grid[a - 1][b] != "." and \
+                            self.grid[a][b - 1] != ".":
                         self.grid[a][b] = '.'
                         x -= 1
-                if a == 0  and b >0 and self.grid[a][b] == "" and b <(self._dimension-1):
-                    if self.grid[a+1][b]!="." and self.grid[a][b+1]!="." and self.grid[a][b-1]:
+                if a == 0 and b > 0 and self.grid[a][b] == "" and b < (self._dimension - 1):
+                    if self.grid[a + 1][b] != "." and self.grid[a][b + 1] != "." and self.grid[a][b - 1]:
                         self.grid[a][b] = '.'
                         x -= 1
-                elif a == self._dimension-1 and b >0 and self.grid[a][b] == "" and b <(self._dimension-1):
-                    if self.grid[a-1][b]!="." and self.grid[a][b-1]!="." and self.grid[a][b+1]:
+                elif a == self._dimension - 1 and b > 0 and self.grid[a][b] == "" and b < (self._dimension - 1):
+                    if self.grid[a - 1][b] != "." and self.grid[a][b - 1] != "." and self.grid[a][b + 1]:
                         self.grid[a][b] = '.'
                         x -= 1
-                elif a == self._dimension-1 and b == 0 and self.grid[a][b] == "":
-                    if self.grid[a-1][b]!="." and self.grid[a][b+1]!=".":
+                elif a == self._dimension - 1 and b == 0 and self.grid[a][b] == "":
+                    if self.grid[a - 1][b] != "." and self.grid[a][b + 1] != ".":
                         self.grid[a][b] = '.'
                         x -= 1
-                elif a == self._dimension-1 and b == self._dimension-1 and self.grid[a][b] == "":
-                    if self.grid[a-1][b]!="." and self.grid[a][b-1]!=".":
+                elif a == self._dimension - 1 and b == self._dimension - 1 and self.grid[a][b] == "":
+                    if self.grid[a - 1][b] != "." and self.grid[a][b - 1] != ".":
                         self.grid[a][b] = '.'
                         x -= 1
-                elif a>0 and a <self._dimension-1 and b == 0:
-                    if self.grid[a-1][b]!="." and self.grid[a][b+1]!="." and self.grid[a+1][b]:
+                elif a > 0 and a < self._dimension - 1 and b == 0:
+                    if self.grid[a - 1][b] != "." and self.grid[a][b + 1] != "." and self.grid[a + 1][b]:
                         self.grid[a][b] = '.'
                         x -= 1
-                elif a > 0 and a < self._dimension - 1 and b == self._dimension-1:
-                    if self.grid[a-1][b]!="." and self.grid[a][b+-1]!="." and self.grid[a+1][b]:
+                elif a > 0 and a < self._dimension - 1 and b == self._dimension - 1:
+                    if self.grid[a - 1][b] != "." and self.grid[a][b + -1] != "." and self.grid[a + 1][b]:
                         self.grid[a][b] = '.'
                         x -= 1
 
 
     def get_grid(self):
         return self.grid
+
+    def get_position(self):
+        for i in range(0, self._dimension):
+            for j in range(0, self._dimension):
+                if self.grid[i][j] == "*":
+                    return i, j
+
+    def move(self, number, direction):
+        i, j = self.get_position()
+        if direction == "up":
+            if i - number < 0:
+                raise Exception("Game over!")
+        if direction == "down":
+            if i + number > self._dimension - 1:
+                raise Exception("Game over!")
+        if direction == "right":
+            if j + number > self._dimension - 1:
+                raise Exception("Game over!")
+        if direction == "left":
+            if j - number < 0:
+                raise Exception("Game over!")
+
+        if direction == "up":
+            for x in range(0, number):
+                if self.grid[i - 1][j] == ".":
+                    self.grid[i - 1][j] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i -= 1
+                    print("da")
+                else:
+                    self.check_game_over(i - 1, j)
+                    self.grid[i - 1][j] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i -= 1
+
+                    self.move_snake_tail()
+
+        elif direction == "down":
+            for x in range(0, number):
+                if self.grid[i + 1][j] == ".":
+                    self.grid[i + 1][j] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i += 1
+
+                else:
+                    self.check_game_over(i + 1, j)
+                    self.grid[i + 1][j] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i += 1
+                    self.move_snake_tail()
+
+        elif direction == "left":
+            for x in range(0, number):
+                if self.grid[i][j - 1] == ".":
+                    self.grid[i][j - 1] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i += 1
+                else:
+                    self.check_game_over(i, j - 1)
+                    self.grid[i][j - 1] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i += 1
+                    self.move_snake_tail()
+        elif direction == "right":
+            for x in range(0, number):
+                if self.grid[i][j + 1] == ".":
+                    self.grid[i][j + 1] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i += 1
+                else:
+                    self.check_game_over(i, j + 1)
+                    self.grid[i][j + 1] = self.grid[i][j]
+                    self.grid[i][j] = "+"
+                    i += 1
+                    self.move_snake_tail()
+    def move_snake_tail(self):
+        pass
+
+    def check_game_over(self, i, j):
+        if self.grid[i][j] == "+":
+            raise Exception("Game over! You hit yourself")
--- ui/console.py
+++ ui/console.py
@@ -3,12 +3,52 @@ class Ui:
         self._dimension = dimension
         self._apples =apples
         self._grid=grid
+        self.direction='up'
     def start(self):
         self.show_grid(self._grid.get_grid())
 
-        while True:
-            cmd= input("Give a command: ")
+        commands={'move':self.ui_move,'up':self.ui_directions,'down':self.ui_directions,'left':self.ui_directions,'right':self.ui_directions}
+        try:
+            while True:
+                cmd= input("Give a command: ")
+                if cmd=='exit':
+                    print("Bye")
+                    break
+                cmd,args = self.get_commands_arguments(cmd)
+                if cmd == "move":
+                    try:
+                        if len(args)>0:
+                            commands[cmd](int(args[0]))
+                        else:
+                            commands[cmd](1)
+                    except ValueError as ve:
+                        print("You need to put a valid int after move")
+                elif cmd == "up":
+                    commands[cmd](cmd)
+                elif cmd == "down":
+                    commands[cmd](cmd)
+                elif cmd == "right":
+                    commands[cmd](cmd)
+                elif cmd == "left":
+                    commands[cmd](cmd)
+                else:
+                    print("Wrong command :D")
+        except Exception as e:
+            print(e)
+    def get_commands_arguments(self,cmd_line):
+        """
+        :param cmd_line:the command with the arguments
+        :return: the command and the arguments separated
+        """
+        pos = cmd_line.find(" ")
+        if pos == -1:  # no arguments were found
+            return cmd_line, []
+        cmd = cmd_line[:pos]
 
+        args = cmd_line[pos + 1:]
+        args = args.split(' ')
+        args = [s.strip() for s in args]
+        return cmd, args
 
     def show_grid(self, grids):
         grid = grids
@@ -31,3 +71,18 @@ class Ui:
                 line += "+---"
             line += "+---"
             print(line)
+    def ui_move(self,number):
+        self._grid.move(number,self.direction)
+        self.show_grid(self._grid.get_grid())
+
+
+    def ui_directions(self,direction):
+        if self.direction =="up" and direction =="down" or self.direction =="down" and direction == "up":
+            print("You cannot make a 180 degree move, try another direction")
+            return
+        elif self.direction =="left" and direction =="right" or self.direction =="right" and direction == "left":
+            print("You cannot make a 180 degree move, try another direction")
+            return
+
+        self.direction = direction
+        self.ui_move(1)
